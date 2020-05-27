3f4ce4d5679cd5c9eca5f19e2b23b49b26fd3469
Déborah <babou.1999@live.fr>
2020-05-27 16:57:18 +0200

    Zut

---
+++ Jeu/Missile.hpp
@@ -0,0 +1,19 @@
+#ifndef MISSILE_HPP
+#define MISSILE_HPP
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Position.hpp"
+
+class Missile{
+    private :
+        Position pos;
+        sf::Texture;
+        sf::Sprite;
+
+    public :
+        Missile(){}
+
+
+};
+#endif
